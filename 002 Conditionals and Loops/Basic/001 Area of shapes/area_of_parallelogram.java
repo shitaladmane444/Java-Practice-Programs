cc0fba87af35033a12eba8920ea3908c6cc290f8
Karan <75921126+karanrao-github@users.noreply.github.com>
2022-11-21 12:57:06 +0530

    Delete 002 Conditionals and Loops/Basic/001 Area of shapes directory

--- 002 Conditionals and Loops/Basic/001 Area of shapes/area_of_parallelogram.java
+++
@@ -1,8 +0,0 @@
-package com.basic.area_01;
-
-public class area_of_parallelogram {
-    public static void main(String[] args) {
-        float b = 6.3f, h=5.6f;
-        System.out.print("Area of Parallelogram is: "+b*h);
-    }
-}
--- 002 Conditionals and Loops/Basic/001 Area of shapes/area_of_rectangle.java
+++
@@ -1,9 +0,0 @@
-package com.basic.area_01;
-
-public class area_of_rectangle {
-    public static void main(String[] args) {
-        float w=12.5f, l=14.3f; // w = width & l = length
-
-        System.out.print("Area of rectangle is: "+w*l);
-    }
-}
