406f2a80f7cf5418bc89f5e3eb16f412ba78388d
Karan <75921126+karanrao-github@users.noreply.github.com>
2022-11-21 12:58:09 +0530

    Added solution files

---
+++ 002 Conditionals and Loops/Basic/001 Area/area_of_rectangle.java
@@ -0,0 +1,9 @@
+package com.basic.area_01;
+
+public class area_of_rectangle {
+    public static void main(String[] args) {
+        float w=12.5f, l=14.3f; // w = width & l = length
+
+        System.out.print("Area of rectangle is: "+w*l);
+    }
+}
---
+++ 002 Conditionals and Loops/Basic/001 Area/area_of_triangle.java
@@ -0,0 +1,10 @@
+package com.basic.area_01;
+
+public class area_of_triangle {
+    public static void main(String[] args) {
+        int b = 7; //base
+        float h = 11.2f; //height
+
+        System.out.println("Area of triangle is: "+h*b/2);
+    }
+}
