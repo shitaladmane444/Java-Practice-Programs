6c601672335ed4ee8490cef61554c0dc96afb75b
Karan <karanrao81441@outlook.com>
2022-12-14 15:29:23 +0530

    Added solution for Discount of product program

---
+++ 003 Functions/Previous Questions solved Using Function/003-conditionals-loops/Basic/Intermediate/inter_p4.java
@@ -0,0 +1,19 @@
+// Calculate Discount of Product
+import java.util.Scanner;
+
+public class inter_p4 {
+
+    static void Discount() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Original Price: ");
+        int og_price = sc.nextInt();
+        System.out.print("Enter the discounted percentage: ");
+        int dis_percentage = sc.nextInt();
+        
+        System.out.print("Discounted Price is: "+ (og_price/dis_percentage)*100);
+
+    }
+    public static void main(String[] args) {
+        Discount();
+    }
+}
